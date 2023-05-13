c75164599a9ee7aa36b4b9ff19aa8b83096fcc84
sp-xd <69642436+SP-XD@users.noreply.github.com>
2023-05-13 21:18:40 +0530

    feat: fetch posts from stream & implement shimmers

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,11 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fistagram/custom_icons_icons.dart';
 import 'package:fistagram/page/search_page.dart';
 import 'package:fistagram/widgets/post_box.dart';
 import 'package:flutter/material.dart';
 import 'package:fistagram/utils/colors.dart';
+import 'package:shimmer/shimmer.dart';
 
 class HomePage extends StatefulWidget {
-  HomePage({Key? key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -37,7 +39,7 @@ class _HomePageState extends State<HomePage> {
     final width = MediaQuery.of(context).size.width;
 
     return Container(
-      padding: EdgeInsets.fromLTRB(width * 0.08, 0, width * 0.08, 0),
+      padding: EdgeInsets.symmetric(horizontal: width * 0.03),
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height,
       child: Scaffold(
@@ -51,7 +53,7 @@ class _HomePageState extends State<HomePage> {
                 Navigator.push(context,
                     MaterialPageRoute(builder: (context) => SearchPage()));
               },
-              icon: Icon(CustomIcons.clarity_search_line,
+              icon: const Icon(CustomIcons.clarity_search_line,
                   size: 30, color: primaryColor)),
           title: const Text(
             'Fistagram',
@@ -67,79 +69,112 @@ class _HomePageState extends State<HomePage> {
           ],
           elevation: 0,
         ),
-        body: SingleChildScrollView(
+        body: ListView(
           physics: const BouncingScrollPhysics(),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 10,
-              ),
-              SizedBox(
-                height: 60,
-                child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    physics: const BouncingScrollPhysics(),
-                    children: [
-                      const SizedBox(width: 6),
-                      //--------------------Upload status button-------------------------
-                      CircleAvatar(
-                          backgroundColor: secondaryBackgroundColor,
-                          radius: 28,
-                          child: IconButton(
-                            padding: EdgeInsets.zero,
-                            icon: const Icon(
-                              CustomIcons.akar_icons_plus,
-                              size: 32,
-                              color: primaryColor,
-                            ),
-                            onPressed: () {},
-                            iconSize: 28,
-                          )),
-                      //---------------------Stories Icon Builder------------------
-                      ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          shrinkWrap: true,
-                          itemCount: instaStories.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            return Row(
-                              children: [
-                                const SizedBox(width: 15),
-                                CircleAvatar(
-                                    child: Image.asset(instaStories[index]),
-                                    radius: 28)
-                              ],
-                            );
-                          }),
-                    ]),
-              ),
-              //-----------------------------Post Feed---------------------------
-              SingleChildScrollView(
-                physics: const BouncingScrollPhysics(),
-                child: Column(
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              height: 60,
+              child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  physics: const BouncingScrollPhysics(),
                   children: [
-                    const SizedBox(
-                      height: 20,
-                    ),
+                    const SizedBox(width: 6),
+                    //--------------------Upload status button-------------------------
+                    CircleAvatar(
+                        backgroundColor: secondaryBackgroundColor,
+                        radius: 28,
+                        child: IconButton(
+                          padding: EdgeInsets.zero,
+                          icon: const Icon(
+                            CustomIcons.akar_icons_plus,
+                            size: 32,
+                            color: primaryColor,
+                          ),
+                          onPressed: () {},
+                          iconSize: 28,
+                        )),
+                    //---------------------Stories Icon Builder------------------
                     ListView.builder(
-                        physics: const NeverScrollableScrollPhysics(),
+                        scrollDirection: Axis.horizontal,
                         shrinkWrap: true,
-                        itemCount: postPics.length,
+                        itemCount: instaStories.length,
                         itemBuilder: (BuildContext context, int index) {
-                          return PostBox(
-                            userId: 'spxd_insta',
-                            imagePath: postPics[index],
-                            likes: 2,
-                            numberOfComments: 1,
+                          return Row(
+                            children: [
+                              const SizedBox(width: 15),
+                              CircleAvatar(
+                                  child: Image.asset(instaStories[index]),
+                                  radius: 28)
+                            ],
                           );
                         }),
-                    const SizedBox(
-                      height: 80,
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
+                  ]),
+            ),
+            //-----------------------------Post Feed---------------------------
+            const SizedBox(
+              height: 20,
+            ),
+            // ListView.builder(
+            //     physics: const NeverScrollableScrollPhysics(),
+            //     shrinkWrap: true,
+            //     itemCount: postPics.length,
+            //     itemBuilder: (BuildContext context, int index) {
+            //       return PostBox(
+            //         userId: 'spxd_insta',
+            //         imagePath: postPics[index],
+            //         likes: 2,
+            //         numberOfComments: 1,
+            //       );
+            //     }),
+            Expanded(
+              child: StreamBuilder(
+                stream:
+                    FirebaseFirestore.instance.collection("posts").snapshots(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Shimmer.fromColors(
+                      baseColor: Colors.grey[300]!,
+                      highlightColor: Colors.grey[100]!,
+                      child: ListView.builder(
+                        itemCount: 2,
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) {
+                          return Card(
+                            elevation: 1.0,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                            child: const SizedBox(height: 300),
+                          );
+                        },
+                      ),
+                    );
+                  }
+
+                  return ListView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return PostBox(
+                          userId: snapshot.data!.docs[index]['username'],
+                          imagePath: snapshot.data!.docs[index]['postUrl'],
+                          profileImage: snapshot.data!.docs[index]['profImage'],
+                          likes: 2,
+                          numberOfComments: 1,
+                        );
+                      });
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 80,
+            )
+          ],
         ),
       ),
     );
